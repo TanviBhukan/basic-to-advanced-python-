1f57ad5e11dc2edfc22c3d1e4609e78142972f00
TanviBhukan <157092275+TanviBhukan@users.noreply.github.com>
2025-10-01 16:40:27 +0530

    circle calculator
    A circle is a set of points in a plane that are at a fixed distance (radius) from a center point.
    Area = π × radius²
    Circumference = 2 × π × radius

---
+++ circlecal.py
@@ -0,0 +1,9 @@
+# python program - circle
+import math
+
+radius = float(input("enter radius of circle: "))
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+
+print("circle area:", area)
+print("circle circumference:", circumference)
