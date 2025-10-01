bfdd476a0b26835763c62af24222fe92ecf9f25d
TanviBhukan <157092275+TanviBhukan@users.noreply.github.com>
2025-10-01 16:39:24 +0530

    rectangle calculating
    A rectangle is a quadrilateral with opposite sides equal and four right angles.
    Area = length × width
    Perimeter = 2 × (length + width)

---
+++ rectanglecal.py
@@ -0,0 +1,10 @@
+# python program - rectangle
+
+length = float(input("enter length of rectangle: "))
+breadth = float(input("enter breadth of rectangle: "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print("rectangle area:", area)
+print("rectangle perimeter:", perimeter)
